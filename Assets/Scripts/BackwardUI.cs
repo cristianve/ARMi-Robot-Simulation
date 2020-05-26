1e01536d1b1a3a7b37b80ffd0ee79bb26374250a
Your Name <you@example.com>
2020-05-26 10:15:02 +0200

    fixed ui parameters

---
+++ Assets/Scripts/BackwardUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackwardUI : MonoBehaviour
+{
+
+    public static int aux = 0;
+    Text BackTargetDistance;
+    Robot robot;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        BackTargetDistance= GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                float distance = GameObject.Find("BaseRobot").GetComponent<Robot>().backtargetDistance;
+                BackTargetDistance.text= "Backward Distance: " + distance;
+    }
+}
---
+++ Assets/Scripts/ForwardUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ForwardUI : MonoBehaviour
+{
+
+    public static int aux = 0;
+    Text ForwardDistance;
+    Robot robot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ForwardDistance= GetComponent<Text>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float distance = GameObject.Find("BaseRobot").GetComponent<Robot>().forwardtargetDistance;
+        ForwardDistance.text= "ForwardDistance: " + distance;
+  
+    }
+}
